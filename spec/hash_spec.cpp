75a9c48a2ae93edb80bcb8de5caebd0a23336acf
Ben Scott <gamepoet@gmail.com>
2019-01-14 22:48:07 -0800

    more hash tests

--- spec/hash_spec.cpp
+++ spec/hash_spec.cpp
@@ -47,17 +47,52 @@ TEST_CASE("hash") {
 
   SECTION("items survive growth") {
     hash_t hash = {};
-    for (uint32_t index = 1; index < 128; ++index) {
+    for (uint32_t index = 1; index < 64; ++index) {
+      hash_insert(&hash, index, index, NULL);
+    }
+    CHECK(hash_capacity(&hash) == 128);
+    for (uint32_t index = 64; index < 128; ++index) {
       hash_insert(&hash, index, index, NULL);
     }
     CHECK(hash_capacity(&hash) == 256);
-    for (uint32_t index = 128; index < 256; ++index) {
-      hash_insert(&hash, index, index, NULL);
-    }
-    CHECK(hash_capacity(&hash) == 512);
-    for (uint32_t index = 1; index < 256; ++index) {
+    for (uint32_t index = 1; index < 128; ++index) {
       CHECK(hash_lookup(&hash, index, 0) == index);
     }
+    hash_free(&hash, NULL);
+  }
+
+  SECTION("hash_lookup gracefully handles an empty table") {
+    hash_t hash = {};
+    CHECK(hash_lookup(&hash, 1, 0) == 0);
+  }
+
+  SECTION("hash_lookup returns the default value if not found") {
+    hash_t hash = {};
+    hash_insert(&hash, 1, 1, NULL);
+    CHECK(hash_lookup(&hash, 2, 0) == 0);
+    hash_free(&hash, NULL);
+  }
+
+  SECTION("hash_contains handles an empty table") {
+    hash_t hash = {};
+    CHECK(!hash_contains(&hash, 1234));
+  }
+
+  SECTION("hash_contains handles return false if not found") {
+    hash_t hash = {};
+    hash_insert(&hash, 1, 1, NULL);
+    CHECK(!hash_contains(&hash, 2));
+    hash_free(&hash, NULL);
+  }
+
+  SECTION("hash_insert does robin hood hashing") {
+    hash_t hash = {};
+    hash_insert(&hash, 1, 1, NULL);
+    hash_insert(&hash, 2, 2, NULL);
+    hash_insert(&hash, 129, 129, NULL);
+    CHECK(hash.keys[1] == 1);
+    CHECK(hash.keys[2] == 129);
+    CHECK(hash.keys[3] == 2);
     hash_free(&hash, NULL);
   }
 }
